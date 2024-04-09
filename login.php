0e1e3a6ef8b6dc3bdec1b48a6e61cc9d92ddf32e
Tiago  Teodoro <119240603+tthheodoro@users.noreply.github.com>
2024-04-09 18:14:13 +0100

    Update

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="bootstrap.css">
+</head>
+<body>
+    <h2>Login</h2>
+    <form action="login.php" method="post">
+        <label for="username">Utilizador:</label><br>
+        <input type="text" id="username" name="username"><br>
+        <label for="password">Password:</label><br>
+        <input type="password" id="password" name="password"><br><br>
+        <button type="submit" class="btn btn-outline-dark" value="Login">Submit</button>
+    </form>
+</body>
+</html>
